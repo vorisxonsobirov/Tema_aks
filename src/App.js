4cd08009d94defc646a98819635164d187e8abe9
vorisxon <vorisxon.me@gmail.com>
2024-05-01 13:34:30 +0500

    nma gap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
  
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-
+      <Navbar/>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import tema_aks from "../assets/temaaks.png"
+import btnAks from "../assets/Снимок_экрана_2024-05-01_132638-removebg-preview.png"
+import "./navbar.css"
 
 const Navbar = () => {
-  return (
-   <>
-   <div className="container">
-        <div className="navbar">
-
-        </div>
-   </div>
-   </>
-  )
+    return (
+        <>
+            <div className="container">
+                <div className="navbar">
+                    <img src={tema_aks} alt="" />
+                    <div className='btn_aks'> <img src={btnAks} alt="" /></div>
+                    <button className="btn_login">ВОЙТИ</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
